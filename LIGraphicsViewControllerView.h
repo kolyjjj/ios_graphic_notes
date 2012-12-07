47b4cd502b1d8118cf7f015923186835ec584ac5
kolyjjj <me.koly.li@gmail.com>
2012-12-07 20:16:24 +0800

    Created an UIView class named LIGraphicsViewControllerView

---
+++ LIGraphicsViewControllerView.h
@@ -0,0 +1,13 @@
+//
+//  LIGraphicsViewControllerView.h
+//  Graphics
+//
+//  Created by koly on 12/7/12.
+//  Copyright (c) 2012 koly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LIGraphicsViewControllerView : UIView
+
+@end
---
+++ LIGraphicsViewControllerView.m
@@ -0,0 +1,31 @@
+//
+//  LIGraphicsViewControllerView.m
+//  Graphics
+//
+//  Created by koly on 12/7/12.
+//  Copyright (c) 2012 koly. All rights reserved.
+//
+
+#import "LIGraphicsViewControllerView.h"
+
+@implementation LIGraphicsViewControllerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
